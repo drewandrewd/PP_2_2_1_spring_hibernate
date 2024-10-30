945c16fe1fde81d891c8c8a07fc8a94c3e9b0237
drewandrewd <andrewid10@gmail.com>
2024-10-30 17:20:12 +0500

    new commit

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -19,6 +20,9 @@ public class MainApp {
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
       userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
       userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User5", "Lastname5", "user5@mail.ru", new Car("Car", 345)));
+      userService.add(new User("User6", "Lastname6", "user6@mail.ru", new Car("CarOne", 346)));
+      userService.add(new User("User7", "Lastname7", "user6@mail.ru", new Car("CarTwo", 347)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,9 +30,14 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         if (user.getCar() != null) {
+            System.out.println("Car model = "+user.getCar().getModel());
+            System.out.println("Car series = "+user.getCar().getSeries());
+         }
          System.out.println();
       }
-
+      User user = userService.getUserByCar("CarOne", 346);
+      System.out.println(user.getFirstName());
       context.close();
    }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -26,4 +26,12 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public User getUserByCar(String model, int series) {
+      String hql = "FROM User u Where u.car.model =: model AND u.car.series =:series";
+      return (User) sessionFactory.getCurrentSession().createQuery(hql)
+              .setParameter("model", model)
+              .setParameter("series", series)
+              .uniqueResult();
+   }
 }
